0e9e1c2ecae05e4235ea64f0ac35f7a592471ba6
andrewassoh <andrew716a@gmail.com>
2025-05-13 17:03:43 -0400

    Update OnlineBookstoreDB.sql

--- OnlineBookstoreDB.sql
+++ OnlineBookstoreDB.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE OnlineBookstoreDB;
 
 CREATE TABLE `authors` (
-   `AuthorID` int PRIMARY KEY,
+   `AuthorID` int AUTO_INCREMENT PRIMARY KEY,
    `AuthorFirstName` varchar(255) DEFAULT NULL,
    `AuthorLastName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `bookauthors` (
-  `BookID` int PRIMARY KEY,
+  `BookID` int AUTO_INCREMENT PRIMARY KEY,
   `AuthorID` int PRIMARY KEY,
   FOREIGN KEY (BookID) REFERENCES books(BookID),
   FOREIGN KEY (AuthorID) REFERENCES authors(AuthorID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `books` (
-  `BookID` int PRIMARY KEY,
+  `BookID` int AUTO_INCREMENT PRIMARY KEY,
   `ISBN` varchar(255) DEFAULT NULL,
   `Title` varchar(255) DEFAULT NULL,
   `Binding` enum('hardcover','paperback') DEFAULT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `books` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `customers` (
-  `CustomerID` int PRIMARY KEY,
+  `CustomerID` int AUTO_INCREMENT PRIMARY KEY,
   `CustomerFirstName` varchar(255) DEFAULT NULL,
   `CustomerLastName` varchar(255) DEFAULT NULL,
   `ShippingAddress` varchar(255) DEFAULT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `inventory` (
-  `InventoryID` int PRIMARY KEY,
+  `InventoryID` int AUTO_INCREMENT PRIMARY KEY,
   `BookID` int DEFAULT NULL,
   `StockQuantity` int DEFAULT NULL,
   `VendorID` int DEFAULT NULL,
@@ -46,30 +46,30 @@ CREATE TABLE `inventory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `invoiceitems` (
-  `InvoiceID` int PRIMARY KEY,
+  `InvoiceID` int AUTO_INCREMENT PRIMARY KEY,
   `BookID` int PRIMARY KEY,
   FOREIGN KEY (InvoiceID) REFERENCES invoices(InvoiceID),
   FOREIGN KEY (BookID) REFERENCES books(BookID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `invoices` (
-  `InvoiceID` int PRIMARY KEY,
+  `InvoiceID` int AUTO_INCREMENT PRIMARY KEY,
   `OrderNumber` int DEFAULT NULL,
   `CustomerID` int DEFAULT NULL,
   FOREIGN KEY (CustomerID) REFERENCES customers(CustomerID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `publishers` (
-  `PublisherID` int PRIMARY KEY,
+  `PublisherID` int AUTO_INCREMENT PRIMARY KEY,
   `PublisherName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `sections` (
-  `SectionID` int PRIMARY KEY,
+  `SectionID` int AUTO_INCREMENT PRIMARY KEY,
   `SectionName` enum('History', 'Children\'s', 'Young Adult', 'Mystery', 'Horror', 'Drama', 'Thriller', 'Science, Technology, & Math', 'Historical Fiction', 'Comics and Graphic Novel', 'Music') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `vendors` (
-  `VendorID` int PRIMARY KEY,
+  `VendorID` int AUTO_INCREMENT PRIMARY KEY,
   `VendorName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
